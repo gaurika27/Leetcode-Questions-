d7bf116dad5ff973d365055ac1afcec59dc5f8d4
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-05-20 23:11:00 +0530

    solution

--- 1021_RemoveOutermostParentheses.cpp
+++ 1021_RemoveOutermostParentheses.cpp
@@ -39,3 +39,17 @@ Constraints:
 s[i] is either '(' or ')'.
 s is a valid parentheses string.
 */
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string res;
+        int opened=0;
+        for(char c: s){
+            if(c=='(' && opened++ > 0) res+=c;
+            if(c==')' && opened-- > 1) res+=c; 
+        }
+        return res;
+        
+    }
+};
