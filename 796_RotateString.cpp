34012e084c7e96e38e76f237eaca5e9e9bde8763
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-05-23 22:58:06 +0530

    solution

--- 796_RotateString.cpp
+++ 796_RotateString.cpp
@@ -21,3 +21,22 @@ Constraints:
 1 <= s.length, goal.length <= 100
 s and goal consist of lowercase English letters.
 */
+class Solution {
+public:
+    void rotate(string &v, int x,int y){
+        for(int i=x,j=y;i<=j;i++,j--){
+            swap(v[i],v[j]);
+        }
+    }
+    bool rotateString(string s, string g) {
+        string k=s;
+        for(int i=0;i<s.size();i++){
+            s=k;
+            rotate(s,s.size()-i,s.size()-1);
+            rotate(s,0,s.size()-1-i);
+            rotate(s,0,s.size()-1);
+            if(s==g) return true;
+        }
+    return false;
+    }
+};
