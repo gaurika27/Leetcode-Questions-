b7019f8c4b1ebe25006e5f58843fc618af613b4e
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-05-24 23:57:28 +0530

    solution

--- 8_StringToInteger.cpp
+++ 8_StringToInteger.cpp
@@ -86,3 +86,36 @@ Constraints:
 s consists of English letters (lower-case and upper-case), digits (0-9), ' ', '+', '-', and '.'.
 */
 
+class Solution {
+public:
+    int myAtoi(string s) 
+    {
+        int i=0;
+        int sign=1;
+        long ans=0;
+        while(i<s.length() && s[i]==' ')
+            i++;
+        if(s[i]=='-')
+        {
+            sign=-1;
+            i++;
+        }
+        else if(s[i]=='+')
+            i++;
+        while(i<s.length())
+        {
+            if(s[i]>='0' && s[i]<='9')
+            {
+                ans=ans*10+(s[i]-'0');
+                if(ans>INT_MAX && sign==-1)
+                    return INT_MIN;
+                else if(ans>INT_MAX && sign==1)
+                    return INT_MAX;
+                i++;
+            }
+            else
+                return ans*sign;
+        }
+        return (ans*sign);
+    }
+};
