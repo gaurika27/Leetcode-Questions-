184a7cb949b4e931a81784acecae02bb9b780c43
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-05-25 00:11:30 +0530

    solution

--- 50_Pow(x,n).cpp
+++ 50_Pow(x,n).cpp
@@ -26,3 +26,22 @@ n is an integer.
 Either x is not zero or n > 0.
 -104 <= xn <= 104
 */
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1.0;
+        long long nn=n;
+        if(nn<0) nn=-1*nn;
+        while(nn){
+            if(nn%2){
+                ans*=x;
+                nn-=1;
+            }else{
+                x*=x;
+                nn/=2;
+            }
+        }
+        if(n<0) ans=(double)(1.0)/(double)(ans);
+        return ans;        
+    }
+};
