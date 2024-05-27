d3f8ee2ce78beea3e340919893e5cd734195e67c
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-05-27 14:31:06 +0530

    solution

--- 1929_ConcatenationOfArray.cpp
+++ 1929_ConcatenationOfArray.cpp
@@ -29,3 +29,18 @@ n == nums.length
 1 <= n <= 1000
 1 <= nums[i] <= 1000
 */
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(2*n);
+        for(int i=0; i<n; ++i){
+           
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+           
+
+        }
+        return ans;       
+    }
+};
