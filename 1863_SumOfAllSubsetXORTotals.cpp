6654f9b237366937f6f01946ebf3661f3be579ea
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-06-01 11:34:56 +0530

    sol

--- 1863_SumOfAllSubsetXORTotals.cpp
+++ 1863_SumOfAllSubsetXORTotals.cpp
@@ -46,3 +46,22 @@ Constraints:
 1 <= nums.length <= 12
 1 <= nums[i] <= 20
 */
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        return dfs(nums, 0, 0);
+    }
+    
+private:
+    int dfs(vector<int>& nums, int index, int currentXor) {
+        // Base case: when all elements have been considered
+        if (index == nums.size()) {
+            return currentXor;
+        }
+        // Include nums[index] in the subset
+        int include = dfs(nums, index + 1, currentXor ^ nums[index]);
+        // Exclude nums[index] from the subset
+        int exclude = dfs(nums, index + 1, currentXor);
+        return include + exclude;
+    }
+};
