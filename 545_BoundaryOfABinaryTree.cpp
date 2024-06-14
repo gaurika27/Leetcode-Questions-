b2fadd4dc10c197168481eb775d754dc775dca81
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-06-14 13:45:58 +0530

    addLeftBoundary

--- 545_BoundaryOfABinaryTree.cpp
+++ 545_BoundaryOfABinaryTree.cpp
@@ -1,10 +1,20 @@
 class Solution{
+  private:
+   void addLeftBoundary(Node* root, vector<int> res){
+    Node* cur=root->left;
+    while(cur){
+      if(!isLeaf(cur)) res.push_back(cur->data);
+      if(cur->left) cur= cur->left;
+      else cur=cur->right;
+    }
+   }
   public:
    vector<int> printBoundary(Node* root){
     vector<int> res;
     if(!root) return res;
     if(!isLeaf(root)) res.push_back(root->data);
     addLeftBoundary(root, res);
+    addLeaves(root, res);
     addRightBoundary(root, res);
     return res;
    }
