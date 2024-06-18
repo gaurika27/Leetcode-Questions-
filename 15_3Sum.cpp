a635f4c9cda5d26b8362630b49410af50dcfb0fd
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-06-18 18:46:07 +0530

    better solution

--- 15_3Sum.cpp
+++ 15_3Sum.cpp
@@ -55,3 +55,27 @@ public:
         return ans;
     }
 };
+
+//Better Solution
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        set<vector<int>> st;
+        for(int i: nums){
+            set<int> hashset;
+            for(int j=i+1; j<n; j++){
+                int third=-(nums[i]+nums[j]);
+                if(hashset.find(third)!=hashset.end()){
+                    vector<int> temp={nums[i], nums[j],third};
+                    sort(temp.begin(), temp.end()); 
+                    st.insert(temp);
+                }
+                hashset.insert(nums[j]);
+
+            }
+        }
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
