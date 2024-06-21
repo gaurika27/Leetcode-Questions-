7d4e8b1bc531cd13257fa9c75a03d91f4ec49286
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-06-21 23:38:09 +0530

    solution

--- 1052_GrumpyBookstoreOwner.cpp
+++ 1052_GrumpyBookstoreOwner.cpp
@@ -30,3 +30,29 @@ n == customers.length == grumpy.length
 0 <= customers[i] <= 1000
 grumpy[i] is either 0 or 1.
 */
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int initialSatisfaction = 0;
+        int maxExtraSatisfaction = 0;
+        int currentWindowSatisfaction = 0;
+        
+        for (int i = 0; i < customers.size(); ++i) {
+            if (grumpy[i] == 0) {
+                initialSatisfaction += customers[i];
+            } else if (i < minutes) {
+                currentWindowSatisfaction += customers[i];
+            }
+        }
+        
+        maxExtraSatisfaction = currentWindowSatisfaction;
+        
+        for (int i = minutes; i < customers.size(); ++i) {
+            currentWindowSatisfaction += customers[i] * grumpy[i];
+            currentWindowSatisfaction -= customers[i - minutes] * grumpy[i - minutes];
+            maxExtraSatisfaction = max(maxExtraSatisfaction, currentWindowSatisfaction);
+        }
+        
+        return initialSatisfaction + maxExtraSatisfaction;   
+    }
+};
