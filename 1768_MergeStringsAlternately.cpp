46058641a165733f512a24f8b3ebd68b78cbfec1
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-06-29 12:34:00 +0530

    sol

--- 1768_MergeStringsAlternately.cpp
+++ 1768_MergeStringsAlternately.cpp
@@ -36,3 +36,21 @@ Constraints:
 1 <= word1.length, word2.length <= 100
 word1 and word2 consist of lowercase English letters.
 */
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res="";
+        int i=0;
+        while(i<word1.length() || i<word2.length()){
+            if(i<word1.length()){
+                res+=word1[i];
+            }
+            if(i<word2.length()){
+                res+=word2[i];
+            }
+            i++;
+        }
+        return res;        
+    }
+};
