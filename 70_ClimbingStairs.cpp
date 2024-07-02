c8787036fb23a64058a53e9cacf796b031ab9a96
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-07-02 18:12:34 +0530

    sol

--- 70_ClimbingStairs.cpp
+++ 70_ClimbingStairs.cpp
@@ -26,3 +26,16 @@ Constraints:
 
 1 <= n <= 45
 */
+
+//Memoization
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        dp[0]=dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];        
+    }
+};
