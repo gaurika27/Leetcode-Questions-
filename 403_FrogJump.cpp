ca6abd6127eae89cc6ac5261661a57785312d52b
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-07-03 19:05:48 +0530

    sol

--- 403_FrogJump.cpp
+++ 403_FrogJump.cpp
@@ -26,3 +26,28 @@ Constraints:
 stones[0] == 0
 stones is sorted in a strictly increasing order.
 */
+
+class Solution {
+public:
+    bool solve(int index , vector<int>& stones ,int n, int k,  map<pair<int,int>,bool>& dp ){
+        if(index>=n) return false;
+        if(index==n-1) return true;
+        if(dp.find({index,k})!=dp.end()){
+            return dp[{index,k}];
+        }
+        bool ans = false;
+        for(int i = max(k-1,1) ; i<=min(k+1,n-1);i++){
+            int j=index;
+            while(j<n && stones[index]+i>stones[j]) j++;
+            if(j<n && stones[index]+i==stones[j]) {
+               ans = ans|| solve(j,stones,n,i,dp);
+            }
+        }
+        return dp[{index,k}]= ans;
+    }
+    bool canCross(vector<int>& stones) {
+        int n = stones.size();
+        map<pair<int,int>,bool> dp;
+        return solve(0,stones, n , 0,dp);
+    }
+};
