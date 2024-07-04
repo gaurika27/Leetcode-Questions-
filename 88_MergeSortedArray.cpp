3756167e13a467266ca6fc4603b88cbb3bb50188
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-07-04 20:49:36 +0530

    m3

--- 88_MergeSortedArray.cpp
+++ 88_MergeSortedArray.cpp
@@ -66,3 +66,26 @@ public:
         }
     }
 };
+
+//M3
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1, j=0;
+        while(i>=0 && j<n){
+            if (nums1[i]>nums2[j]){
+                swap(nums1[i],nums2[j]);
+                i--;
+                j++;
+            }
+            else{
+                break;
+            }
+        }
+        sort(nums1.begin(), nums1.end()-n);
+        sort(nums2.begin(), nums2.end());
+        for(int i=m;i<n+m;i++){
+            nums1[i]=nums2[i-m];
+        }
+    }
+};
