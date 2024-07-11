f66ae3057d47dfc723b6cbb052820644852fd0b2
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-07-11 14:36:53 +0530

    M2 and M3

--- 200_NumberOfIslands.cpp
+++ 200_NumberOfIslands.cpp
@@ -78,3 +78,99 @@ public:
         return numIslands;
     }
 };
+
+//M2
+class Solution {
+private:
+    void bfs(vector<vector<char>>& grid, int i, int j, vector<vector<int>>& vis) {
+        vis[i][j] = 1;
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        int row = grid.size();
+        int col = grid[0].size();
+        int delrow[4] = {-1, 0, 1, 0};
+        int delcol[4] = {0, 1, 0, -1};
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for (int k = 0; k < 4; k++) {
+                int newrow = r + delrow[k];
+                int newcol = c + delcol[k];
+
+                if (newrow >= 0 && newrow < row && newcol >= 0 &&
+                    newcol < col && grid[newrow][newcol] == '1' &&
+                    !vis[newrow][newcol]) {
+                    vis[newrow][newcol] = 1;
+                    q.push({newrow, newcol});
+                }
+            }
+        }
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && grid[i][j] == '1') {
+                    bfs(grid, i, j, vis);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+//M3(but doesn't work)
+class Solution {
+private:
+    void bfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid){
+        vis[row][col]=1;
+        queue<pair<int,int>> q;
+        q.push({row, col});
+        int n=grid.size();
+        int m=grid[0].size();
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+
+            //traverse in its neighbours and mark them if its an islan
+            for(int delrow=-1; delrow<=1; delrow++){
+                for(int delcol=-1; delcol<=1; delcol++){
+                    if (delrow == 0 && delcol == 0) continue;
+                    int nrow=row+delrow;
+                    int ncol=col+delcol;
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
+                        vis[nrow][ncol]=1;
+                        q.push({nrow, ncol});
+                    }  
+                }
+            }
+        }
+
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int cnt=0;
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        for(int row=0; row<n; row++){
+            for(int col=0; col<m; col++){
+                if(!vis[row][col] && grid[row][col]=='1'){
+                    cnt++;
+                    bfs(row, col, vis, grid);
+                }
+            }
+        }        
+        return cnt;
+    }
+};
