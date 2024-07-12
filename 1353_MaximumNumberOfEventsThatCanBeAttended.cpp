aa64c7bc8c91ea4279de13d6c13589f39250a93a
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-07-13 01:40:13 +0530

    greedy

---
+++ 1353_MaximumNumberOfEventsThatCanBeAttended.cpp
@@ -0,0 +1,69 @@
+/*
+You are given an array of events where events[i] = [startDayi, endDayi]. Every event i starts at startDayi and ends at endDayi.
+
+You can attend an event i at any day d where startTimei <= d <= endTimei. You can only attend one event at any time d.
+
+Return the maximum number of events you can attend.
+
+ 
+
+Example 1:
+
+
+Input: events = [[1,2],[2,3],[3,4]]
+Output: 3
+Explanation: You can attend all the three events.
+One way to attend them all is as shown.
+Attend the first event on day 1.
+Attend the second event on day 2.
+Attend the third event on day 3.
+Example 2:
+
+Input: events= [[1,2],[2,3],[3,4],[1,2]]
+Output: 4
+ 
+
+Constraints:
+
+1 <= events.length <= 105
+events[i].length == 2
+1 <= startDayi <= endDayi <= 105
+*/
+
+class Solution {
+public:
+	static const bool mySort(vector<int> &a, vector<int> &b) {
+		if (a[0] != b[0]) {
+			return a[0] < b[0];
+		}
+		return a[1] < b[1];
+	}
+
+	int maxEvents(vector<vector<int>>& events) {
+		sort(events.begin(), events.end(), mySort);
+
+		int n = events.size();
+		int ans = 0;
+		int i = 0;
+
+		multiset<int> s;
+		for (int d = 1; d < 100001; ++d) {
+			while (!s.empty() && *(s.begin()) < d) {
+				s.erase(s.begin());
+			}
+
+			while (i < n && events[i][0] == d) {
+				s.insert(events[i][1]);
+				i++;
+			}
+
+			if (s.size() > 0) {
+				ans++;
+				s.erase(s.begin());
+			}
+		}
+
+		return ans;
+
+	}
+};
