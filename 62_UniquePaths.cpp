be54fb4c0b3db1fb99fdfee21e09333b3288e095
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-07-19 15:06:39 +0530

    sol

--- 62_UniquePaths.cpp
+++ 62_UniquePaths.cpp
@@ -26,7 +26,7 @@ Constraints:
 
 1 <= m, n <= 100
 */
-
+//M1
 class Solution {
 public:
 
@@ -43,6 +43,33 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
         return f(m-1,n-1,dp);
+
+        
+    }
+};
+//M2
+class Solution {
+public:
+
+
+    int f(int m, int n,vector<vector<int>>& dp){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up=0, left=0;
+                if(i>0) up=dp[i-1][j];
+                if(j>0) left=dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return f(m,n,dp);
 
         
     }
